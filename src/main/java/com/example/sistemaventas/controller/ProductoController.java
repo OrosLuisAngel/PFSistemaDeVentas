3abfb358fad2eae39b9e29130c6d15a5147c7de3
OrosLuisAngel <luis70oros@gmail.com>
2025-10-04 21:02:56 -0500

    Creaccion del modulo de productos para controller

--- src/main/java/com/example/sistemaventas/controller/ProductoController.java
+++ src/main/java/com/example/sistemaventas/controller/ProductoController.java
@@ -1,20 +1,8 @@
 package com.example.sistemaventas.controller;
+import org.springframework.stereotype.Controller;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Controller
 public class ProductoController {
 
-    private Integer id;
-    private String nombre;
-    private Double precio;
-    private Integer stock;
-    private String descripcion;
 
 }
