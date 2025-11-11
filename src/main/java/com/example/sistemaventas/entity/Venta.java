a6477fbb3f9b0b01181ff92f82ef20c538b063c1
OrosLuisAngel <luis70oros@gmail.com>
2025-11-11 18:42:49 -0500

    Se agregaron nuevas paginas de Usuarios, Reportes, Productos y Catalogo (Aun se haran combios), ademas de eso se solucionaron errores de nivel basico en las entidades Rol y Ventas

--- src/main/java/com/example/sistemaventas/entity/Venta.java
+++ src/main/java/com/example/sistemaventas/entity/Venta.java
@@ -1,6 +1,9 @@
 package com.example.sistemaventas.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer ventaID;
     private String fecha;
     private Double total;
